054f354e094e490cba4fbfa1d87d0132dafacad1
Vincentvandijk96 <138885918+Vincentvandijk96@users.noreply.github.com>
2023-09-20 10:20:22 +0200

    Create script.js

---
+++ web/script.js
@@ -0,0 +1,48 @@
+const accessToken = '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';
+const repoOwner = 'Vincentvandijk96';
+const repoName = 'WOOSETUP';
+const pathToDocs = 'docs/'; // Het pad naar de map met PDF-bestanden
+
+const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${pathToDocs}`;
+
+async function fetchPDFs() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        const pdfList = document.getElementById('pdf-list');
+        const searchInput = document.getElementById('search-input');
+
+        function filterPDFs() {
+            const searchTerm = searchInput.value.toLowerCase();
+            pdfList.innerHTML = ''; // Leeg de lijst voordat je opnieuw gaat filteren
+            
+            data.forEach(item => {
+                if (item.name.endsWith('.pdf')) {
+                    const fileName = item.name.toLowerCase();
+                    if (fileName.includes(searchTerm)) {
+                        const listItem = document.createElement('li');
+                        const link = document.createElement('a');
+                        // Pas de link aan om naar GitHub Pages te verwijzen
+                        link.href = `https://${repoOwner}.github.io/${repoName}/${item.name}`;
+                        link.textContent = item.name;
+                        listItem.appendChild(link);
+                        pdfList.appendChild(listItem);
+                    }
+                }
+            });
+        }
+
+        searchInput.addEventListener('input', filterPDFs);
+
+        // Toon alle PDF's bij het starten van de pagina
+        filterPDFs();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+fetchPDFs();
+
+
+
