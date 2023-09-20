39993b0f1c2ee92b4971746901564a515febd7bd
Vincentvandijk96 <138885918+Vincentvandijk96@users.noreply.github.com>
2023-09-20 09:32:24 +0200

    Create script.js

---
+++ web/script.js
@@ -0,0 +1,29 @@
+const accessToken = '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';
+const repoOwner = 'https://github.com/Vincentvandijk96';
+const repoName = 'WOOSETUP';
+
+const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/`;
+
+async function fetchPDFs() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        const pdfList = document.getElementById('pdf-list');
+        
+        data.forEach(item => {
+            if (item.name.endsWith('.pdf')) {
+                const listItem = document.createElement('li');
+                const link = document.createElement('a');
+                link.href = item.html_url;
+                link.textContent = item.name;
+                listItem.appendChild(link);
+                pdfList.appendChild(listItem);
+            }
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+fetchPDFs();
