794752319c68862da869f4a84fb6fef845f27987
Vincentvandijk96 <138885918+Vincentvandijk96@users.noreply.github.com>
2023-09-22 09:10:58 +0200

    Update scriptp.js

--- docs/scriptp.js
+++ docs/scriptp.js
@@ -2,57 +2,60 @@ const accessToken = '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';
 const repoOwner = 'Vincentvandijk96';
 const repoName = 'WOOSETUP';
 const pathToDocs = 'docs/'; // Het pad naar de map met PDF-bestanden
-const perPage = 2;
+const perPage = 2; // Aantal documenten per pagina
+
 let currentPage = 1;
+let searchTerm = '';
 
-const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${pathToDocs}?per_page=${perPage}&page=${currentPage}`;
-
-async function fetchPDFs() {
+async function fetchPDFs(page) {
     try {
+        const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${pathToDocs}?page=${page}&per_page=${perPage}`;
         const response = await fetch(apiUrl);
         const data = await response.json();
 
         const pdfList = document.getElementById('pdf-list');
-        const searchInput = document.getElementById('searchInput');
+        pdfList.innerHTML = ''; // Leeg de lijst voordat je documenten toevoegt
 
-        function filterPDFs() {
-            const searchTerm = searchInput.value.toLowerCase();
-            pdfList.innerHTML = ''; // Leeg de lijst voordat je opnieuw gaat filteren
-            
-            data.forEach(item => {
-                if (item.name.endsWith('.pdf')) {
-                    const fileName = item.name.toLowerCase();
-                    if (fileName.includes(searchTerm)) {
-                        const listItem = document.createElement('li');
+        data.forEach(item => {
+            if (item.name.endsWith('.pdf')) {
+                const listItem = document.createElement('li');
 
-                        // Maak een div voor de prefix en voeg deze toe aan het lijstitem
-                        const prefixDiv = document.createElement('div');
-                        prefixDiv.classList.add('prefix');
-                        prefixDiv.textContent = item.name.split('-')[0]; // Neem het deel voor het eerste "-"
-                        listItem.appendChild(prefixDiv);
+                // Maak de link naar het PDF-bestand en voeg deze toe aan het lijstitem
+                const link = document.createElement('a');
+                link.href = `https://${repoOwner}.github.io/${repoName}/${item.name}`;
+                link.textContent = item.name;
+                listItem.appendChild(link);
 
-                        // Maak de link naar het PDF-bestand en voeg deze toe aan het lijstitem
-                        const link = document.createElement('a');
-                        link.href = `https://${repoOwner}.github.io/${repoName}/${item.name}`;
-                        link.textContent = item.name.split('-').slice(1).join('-'); // Neem het deel na het eerste "-"
-                        listItem.appendChild(link);
-
-                        pdfList.appendChild(listItem);
-                    }
-                }
-            });
-        }
-
-        searchInput.addEventListener('input', filterPDFs);
-
-        // Toon alle PDF's bij het starten van de pagina
-        filterPDFs();
+                pdfList.appendChild(listItem);
+            }
+        });
     } catch (error) {
         console.error(error);
     }
 }
 
-fetchPDFs();
+function filterAndLoadPDFs() {
+    currentPage = 1; // Reset naar pagina 1 bij elke nieuwe zoekopdracht
+    searchTerm = document.getElementById('searchInput').value.toLowerCase();
+    fetchPDFs(currentPage);
+}
+
+document.getElementById('searchInput').addEventListener('input', filterAndLoadPDFs);
+
+document.getElementById('nextPageBtn').addEventListener('click', () => {
+    currentPage++;
+    fetchPDFs(currentPage);
+});
+
+document.getElementById('prevPageBtn').addEventListener('click', () => {
+    if (currentPage > 1) {
+        currentPage--;
+        fetchPDFs(currentPage);
+    }
+});
+
+// Toon alle PDF's bij het starten van de pagina
+fetchPDFs(currentPage);
 
 // Maak een nieuw link-element aan om het CSS-bestand te laden
 var link = document.createElement("link");
