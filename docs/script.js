7e417577cffbdbc8930f007c5a589bf22fc03210
Vincentvandijk96 <138885918+Vincentvandijk96@users.noreply.github.com>
2023-10-11 15:54:57 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,6 @@
 const repoOwner = 'Vincentvandijk96';
 const repoName = 'WOOSETUP';
-const pathToDocs = 'pdf/'; // Het pad naar de map met PDF-bestanden
+const pathToDocs = 'docs/'; // Het pad naar de map met PDF-bestanden
 const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${pathToDocs}`;
 
 async function fetchPDFs() {
