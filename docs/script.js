1e67263175f93a054a59f1d7c4bce856049aff83
Vincentvandijk96 <138885918+Vincentvandijk96@users.noreply.github.com>
2023-10-24 16:24:27 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -58,21 +58,30 @@ link.rel = "stylesheet";
 link.type = "text/css";
 link.href = "https://vincentvandijk96.github.io/woopublicatie/style.css"; // Vervang dit door het pad naar jouw CSS-bestand
 
-//voer script uit
-    fetchPDFs();
-
-// Voeg het css in
-    document.head.appendChild(link);
-
-
-if (document.getElementById('pdf-list')) {
-    // voer hier je functie uit
 
 
 
-    
+// Event listener die wordt geactiveerd wanneer de pagina is geladen
+window.addEventListener('load', function() {
+    // De gewenste URL waar je naar wilt controleren
+    var gewensteUrl = 'https://coevorden.acc.openonline.weareyou.io/woo-publicaties';
 
-}
+    // Huidige URL van de webpagina
+    var huidigeUrl = window.location.href;
 
+    // Controleer of de huidige URL overeenkomt met de gewenste URL
+    if (huidigeUrl === gewensteUrl) {
+        console.log('Je bent op de gewenste URL.');
+        // Voer hier de gewenste acties uit als de URL overeenkomt
+            //voer script uit
+            fetchPDFs();
+
+            // Voeg het css in
+            document.head.appendChild(link);
+    } else {
+        console.log('Je bent niet op de gewenste URL.');
+        // Voer hier de gewenste acties uit als de URL niet overeenkomt
+    }
+});
 
 
