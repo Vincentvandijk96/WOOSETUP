db4571bef71ae3ee386d509e486bf217838ec85e
Vincentvandijk96 <138885918+Vincentvandijk96@users.noreply.github.com>
2023-11-15 11:26:22 +0100

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,68 +1,58 @@
-const repoOwner = 'Vincentvandijk96';
-const repoName = 'woodocumenten';
-const pathToDocs = 'docs/'; // Het pad naar de map met PDF-bestanden
+const repoOwner = 'OpenCoevorden';
+const repoName = 'publicaties';
+const pathToDocs = 'docs/';
 const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${pathToDocs}`;
+
+let data = [];
 
 async function fetchPDFs() {
     try {
         const response = await fetch(apiUrl);
-        const data = await response.json();
+        data = await response.json();
 
         console.log('Opgehaalde gegevens:', data);
-
-        const pdfList = document.getElementById('pdf-list');
-        const searchInput = document.getElementById('searchInput');
-
-        function filterPDFs() {
-            const searchTerm = searchInput.value.toLowerCase();
-            pdfList.innerHTML = ''; // Leeg de lijst voordat je opnieuw gaat filteren
-            
-            data.forEach(item => {
-                if (item.name.endsWith('.pdf')) {
-                    const fileName = item.name.toLowerCase();
-                    if (fileName.includes(searchTerm)) {
-                        const listItem = document.createElement('li');
-
-                        // Maak een div voor de prefix en voeg deze toe aan het lijstitem
-                        const prefixDiv = document.createElement('div');
-                        prefixDiv.classList.add('prefix');
-                        prefixDiv.textContent = item.name.split(' ')[0]; // Neem het deel voor het eerste "-"
-                        listItem.appendChild(prefixDiv);
-
-                        // Maak de link naar het PDF-bestand en voeg deze toe aan het lijstitem
-                        const link = document.createElement('a');
-                        link.href = `https://${repoOwner}.github.io/${repoName}/${item.name}`;
-                        link.textContent = item.name.split(' ').slice(1).join(' '); // Neem het deel na het eerste "-"
-                        link.target = '_blank'; // Open de link in een nieuw tabblad
-                        listItem.appendChild(link);
-
-                        pdfList.appendChild(listItem);
-                    }
-                }
-            });
-        }
-
-        searchInput.addEventListener('input', filterPDFs);
 
         // Toon alle PDF's bij het starten van de pagina
         filterPDFs();
     } catch (error) {
         console.error(error);
+        // Voeg hier code toe om de gebruiker te informeren over de fout
     }
 }
 
+function filterPDFs() {
+    const pdfList = document.getElementById('pdf-list');
+    const searchInput = document.getElementById('searchInput');
+    const searchTerm = searchInput.value.toLowerCase();
+    pdfList.innerHTML = '';
 
-// Maak een nieuw link-element aan om het CSS-bestand te laden
-var link = document.createElement("link");
-link.rel = "stylesheet";
-link.type = "text/css";
-link.href = "https://vincentvandijk96.github.io/woopublicatie/style.css"; // Vervang dit door het pad naar jouw CSS-bestand
+    data.forEach(item => {
+        if (item.name.endsWith('.pdf')) {
+            const fileName = item.name.toLowerCase();
+            if (fileName.includes(searchTerm)) {
+                addListItem(pdfList, item);
+            }
+        }
+    });
+}
 
+function addListItem(pdfList, item) {
+    const listItem = document.createElement('li');
 
-            //voer script uit
-            fetchPDFs();
+    const prefixDiv = document.createElement('div');
+    prefixDiv.classList.add('prefix');
+    prefixDiv.textContent = item.name.split(' ')[0];
+    listItem.appendChild(prefixDiv);
 
-            // Voeg het css in
-            document.head.appendChild(link);
+    const link = document.createElement('a');
+    link.href = `https://opencoevorden.nl/${item.name}`;
+    link.textContent = item.name.split(' ').slice(1).join(' ');
+    link.target = '_blank';
+    listItem.appendChild(link);
 
+    pdfList.appendChild(listItem);
+}
 
+document.getElementById('searchInput').addEventListener('input', filterPDFs);
+
+fetchPDFs();
